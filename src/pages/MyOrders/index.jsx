bfc943ae04224de00613e32af9003275c66af524
Victor Castañeda <victorarroyavecastaneda@gmail.com>
2024-10-26 21:26:35 -0500

    update styles for orders list

--- src/pages/MyOrders/index.jsx
+++ src/pages/MyOrders/index.jsx
@@ -4,6 +4,7 @@ import { ShoppingContext } from '../../context'
 import { Layout } from '../../components/Layout'
 import { Navbar } from '../../components/Navbar'
 import { OrdersCard } from '../../components/OrdersCard'
+import './styles.css'
 
 function MyOrders() {
   const context = useContext(ShoppingContext)
@@ -13,8 +14,9 @@ function MyOrders() {
       <Navbar />
       <Layout>
       <div className="back-icon__container mb-10 flex gap-3 items-center">
-          <h2 className='font-noto'>My orders</h2>
-        </div>
+        <h2 className='font-noto'>My orders</h2>
+      </div>
+        <main className="list-container__orders">
         {
           context.order.map((order, index) => (
             <Link to={`/my-orders/product/?id=${index}`} key={index}>
@@ -22,6 +24,7 @@ function MyOrders() {
             </Link>
           ))
         }
+        </main>
       </Layout>
     </>
   )
