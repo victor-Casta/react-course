ea1e122d501925993f5f8c8baca2bcb7cd0f89da
Victor Castañeda <victorarroyavecastaneda@gmail.com>
2024-10-31 18:44:06 -0500

    content for pages of sub categories

--- src/pages/Sale/index.jsx
+++ src/pages/Sale/index.jsx
@@ -1,15 +1,37 @@
+import { useContext } from "react"
+import { ShoppingContext } from "../../context"
 import { Layout } from "../../components/Layout"
+import { Card } from '../../components/Card'
 import { Navbar } from "../../components/Navbar"
 
 function Sale() {
+  const context = useContext(ShoppingContext)
+
   return (
     <>
       <Navbar />
       <Layout>
-        Sale
+        <h2 className="mb-10 mt-3 font-fraunces font-black text-3xl uppercase text-black">Sale</h2>
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(290px,1fr))] gap-3 justify-center items-center">
+          {
+            context.products?.filter(item => item.subCategory.includes('sale'))
+            .map(item =>
+              <Card
+                price={item.price}
+                title={item.name}
+                key={item.id}
+                image={item.image}
+                category={item.category}
+                description={item.description}
+                id={item.id}
+                pricePerUnit={item.pricePerUnit} />
+            )
+          }
+        </div>
       </Layout>
     </>
   )
 }
+
 
 export { Sale }
